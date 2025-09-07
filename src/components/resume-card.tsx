0f2f381324e5227a41eaab467918e0ccdcf949e1
prokittikun <pro.kittikun.2546@gmail.com>
2025-09-07 16:09:18 +0700

    feat: update description prop type to support React nodes

--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -17,7 +17,7 @@ interface ResumeCardProps {
   href?: string;
   badges?: readonly string[];
   period: string;
-  description?: string;
+  description?: string | React.ReactNode;
   defaultExpanded?: boolean;
 }
 export const ResumeCard = ({
